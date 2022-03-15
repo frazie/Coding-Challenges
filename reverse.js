9d86e0d1a520888b07ddefd9c0b72ba6024dcfa3
Frazier Odhiambo <ofrazy@gmail.com>
2022-03-16 01:17:32 +0300

    coding challenge

---
+++ reverse.js
@@ -0,0 +1,15 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+// solution
+function solution(str){
+    return str.split('').reverse().join('')
+  }
+
+//   test 
+//   solution('world'), 'dlrow');
+//   solution('hello'), 'olleh');
+//   solution(''), '');
+//   solution('h'), 'h');
