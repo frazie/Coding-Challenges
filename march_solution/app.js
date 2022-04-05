d01505afb18cef615cc0229186801f2317794916
Frazier Odhiambo <ofrazy@gmail.com>
2022-04-05 16:43:47 +0300

    more

---
+++ march_solution/app.js
@@ -0,0 +1,13 @@
+// Write a function called repeatStr which repeats the given string string exactly n times.
+
+// repeatStr(6, "I") // "IIIIII"
+// repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+// my solution 
+
+function repeatStr (n, s) {
+    a = s.repeat(n);
+      return a;
+    }
+
+    // here we will use the repeat function for the solution
