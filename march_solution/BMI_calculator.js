e454faa0ef82820546d02116bf827f7cebd74f84
Frazier Odhiambo <ofrazy@gmail.com>
2022-04-09 01:20:58 +0300

    calculation of bmi

---
+++ march_solution/BMI_calculator.js
@@ -0,0 +1,27 @@
+
+// Coding challenge 
+
+// Calculate BMI 
+// Write function bmi that calculates body mass index (bmi = weight / height2).
+
+// if bmi <= 18.5 return "Underweight"
+
+// if bmi <= 25.0 return "Normal"
+
+// if bmi <= 30.0 return "Overweight"
+
+// // if bmi > 30 return "Obese"
+
+// My Solution 
+function bmi(weight, height) {
+    let mass = weight / (height * height);
+    if (mass <=18.5) {
+    return "Underweight";
+    } else if (mass <=25.0) {
+    return "Normal";
+    } else if (mass <=30.0) {
+    return "Overweight";
+    } else {
+    return "Obese";
+   };
+   }
