51c0273140ad7ad897b22d46a444be156a8714dd
Frazier Odhiambo <ofrazy@gmail.com>
2022-04-09 09:16:13 +0300

    third angle of a triangle? sure!

---
+++ march_solution/angles.js
@@ -0,0 +1,22 @@
+// Coding challenge
+
+// #######third angle of a triangle #######
+
+
+// You are given two interior angles (in degrees) of a triangle.
+
+// Write a function to return the 3rd.
+
+// Note: only positive integers will be tested.
+
+
+// ########My Solution#####
+
+function otherAngle(a, b) {
+  
+    let fullAngle = 180
+    
+    return fullAngle - a - b;
+  }
+
+
