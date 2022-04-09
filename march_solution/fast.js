f37531dbfdd035a9c41002ec2f025d6ea9bc6361
Frazier Odhiambo <ofrazy@gmail.com>
2022-04-09 08:55:14 +0300

    coding under pressure? suregit add fast.js

---
+++ march_solution/fast.js
@@ -0,0 +1,12 @@
+// coding challenge
+
+// You cant code under pressure #1
+
+// Code as fast as you can! You need to double the integer and return it.
+
+
+// my solution 
+function doubleInteger(i) {
+    let product = i*2;
+     return product;
+   }
