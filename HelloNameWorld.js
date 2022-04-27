6e5af5dccd37877e05ba9e6ffa86998acee959f0
Frazier Odhiambo <ofrazy@gmail.com>
2022-04-27 13:53:57 +0300

    daily challenge

---
+++ HelloNameWorld.js
@@ -0,0 +1,17 @@
+Define a method hello that returns "Hello,Name!" to a given name, or says Hello, World! if name is not given(or passed as an empry string).Define
+
+Assuming that name is a string and it checks for user typose to return a name with a first capital letter (Xxxx)
+
+
+solution
+
+function hello(name){
+    if (name == !null)
+    {
+        return `Hello, ${name[0],toUppercase() + name.slice(1).toLowerCase()}!`
+    }else {
+        return "Hello, World!"
+    }
+
+    
+}
