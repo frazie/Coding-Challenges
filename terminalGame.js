2ad77f34a09f0d88d271d682cd458b1cf2f3461e
Frazier Odhiambo <ofrazy@gmail.com>
2022-04-28 13:33:22 +0300

    daily challenge

---
+++ terminalGame.js
@@ -0,0 +1,25 @@
+// Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
+
+solution
+
+function combat(health, damage) {
+    // Write your code here
+    const newHealth = health-damage
+    
+    if (newHealth >= 0){
+      return newHealth
+    }else {
+      return 0
+    }
+}
+
+// or 
+
+function combat(health, damage) {
+    return health - damage < 0 ? 0 : health - damage;
+  }
+
+// test 
+// combat(100, 5), 95);
+// combat(92, 8), 84);
+// combat(20, 30), 0, "Health cannot go below 0");
