525884381359e9165263ba21180c048173563170
Frazier Odhiambo <ofrazy@gmail.com>
2022-05-04 01:42:45 +0300

    daily challenge

---
+++ debugSayHello.js
@@ -0,0 +1,20 @@
+// Debugging sayHello function
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+// Example output:
+
+// Hello, Mr. Spock
+
+// function sayHello(name) {
+//     return 'Hello'
+//   }
+
+//   solution
+
+function sayHello(name){
+    return `Hello, ${name}`
+}
+
+// test
+// ('Captain Kirk'), 'Hello, Captain Kirk'
+// ('Liutenant Uhura'), 'Hello, Liutenant Uhura'
