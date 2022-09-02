03516e229a2cc3bf71b1a6ae84ab5c2a65159ab8
Frazier Odhiambo <ofrazy@gmail.com>
2022-09-02 21:29:48 +0300

    coding challenge

---
+++ marks!.js
@@ -0,0 +1,26 @@
+// Remove all exclamation marks from the end of sentence.
+
+// Examples
+// remove("Hi!") === "Hi"
+// remove("Hi!!!") === "Hi"
+// remove("!Hi") === "!Hi"
+// remove("!Hi!") === "!Hi"
+// remove("Hi! Hi!") === "Hi! Hi"
+// remove("Hi") === "Hi"
+
+// solution
+function remove(s){
+    return s.replace(/!+$/, '');
+  }
+
+  or 
+
+  function remove (string) {  
+    string = string.split("")
+    
+    for(let i=0; i<=string.length; i++){
+      if (string[string.length-1]=="!")
+    string.pop()
+    }  
+    return string.join("")
+  }
