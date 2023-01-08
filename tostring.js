f68543c229b3b1708f922570878a298dda67ad67
Frazier Odhiambo <ofrazy@gmail.com>
2023-01-08 10:19:18 +0300

    coding challenge

---
+++ tostring.js
@@ -0,0 +1,19 @@
+// DESCRIPTION:
+// Input: Array of elements
+
+// ["h","o","l","a"]
+
+// Output: String with comma delimited elements of the array in th same order.
+
+// "h,o,l,a"
+
+// solution 
+
+function printArray(array){
+//   const leta = []
+//   array.map(x => leta.push(x) )
+  
+//   return leta.join(',')
+  
+  return array.join(',')
+}
